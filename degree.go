593f9ab49f7f26cffc2b803cc6b99b2c610d677c
sujinsr <sujinsr@gmail.com>
2015-06-09 19:24:02 +0530

    Added code to build path

--- degree.go
+++ degree.go
@@ -6,7 +6,6 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	//"time"
 )
 
 const (
@@ -42,20 +41,31 @@ type Person struct {
 	Movies []Movie `json:"movies"`
 }
 
+/* For back trace */
+type Path struct {
+	name  string
+	movie Movie
+}
+
+var trace map[string]Path
+var processedMovie map[string]bool
+var processedPerson map[string]bool
+var degree int
+
 func formUrl(url string) string {
 	url = homeurl + url
 	return url
 }
 
-
-func getCastCrew(name string) (CastCrew, error) {
-	url := formUrl(name)
+func getRoleFromMovie(movie string, person string) CCtype {
+	url := formUrl(movie)
 	var cc CastCrew
+	var ccrole CCtype
 
 	res, err := http.Get(url)
 	if err != nil {
 		fmt.Println(err)
-		return cc, err
+		return ccrole
 	}
 	defer res.Body.Close()
 
@@ -68,23 +78,45 @@ func getCastCrew(name string) (CastCrew, error) {
 	if err != nil {
 		//fmt.Println("json decode error", err)
 	}
-
-	return cc, err
+	for _, ccrole = range cc.Cast {
+		if ccrole.Url == person {
+			goto tag
+		}
+	}
+	for _, ccrole = range cc.Crew {
+		if ccrole.Url == person {
+			goto tag
+		}
+	}
+tag:
+	return ccrole
 }
 
-func getCastCrewList(movie_list []Movie) []CCtype {
-	cclist := make([]CCtype, 0)
+func backtrace(personA string, personB string) {
+	var prevname string
+	path := make([]Path, 0)
+	path = append(path, trace[personB])
+	br := 0
 
-	for _, val := range movie_list {
-		//fmt.Println ("CastCrew", i, val)
-		cc, err := getCastCrew(val.Url)
-		if err != nil {
-			continue
-		}
-		cclist = append(cclist, cc.Cast...)
-		cclist = append(cclist, cc.Crew...)
+	for path[len(path) - 1].name != personA && br < degree-1 {
+		path = append(path, trace[path[len(path) - 1].name])
+		br++
 	}
-	return cclist
+
+	/*Print Path */
+	for i, v := range path {
+		fmt.Println("Movie:", v.movie.Url)
+		if i == 0 {
+			role := getRoleFromMovie(path[0].movie.Url, personB)
+			fmt.Println(role.Role, ":", personB)	
+		} else {
+			role := getRoleFromMovie(v.movie.Url, prevname) 	
+			fmt.Println(role.Role, ":", prevname)
+		}
+			
+		fmt.Println(v.name, ":", v.movie.Role, "\n")
+		prevname = v.name
+	}
 }
 
 func getPersonMovies(name string) ([]Movie, error) {
@@ -125,28 +157,131 @@ func getPersonMovieList(person_list []CCtype) []Movie {
 
 }
 
-func processPerson(personA string, personB string) {
-	degree := 1
-	movie_list, _ := getPersonMovies(personA)
-	castcrew_list := getCastCrewList(movie_list)
-	for degree <= 6 {
-		for _, cc := range castcrew_list {
-			//fmt.Println(i, cc)
-			if cc.Url == personB {
-				fmt.Println("Degree of Seperation Between\n\t", personA, "&", personB, "-", degree)
-				return
+func getCastCrew(name string) (CastCrew, error) {
+	url := formUrl(name)
+	var cc CastCrew
+
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return cc, err
+	}
+	defer res.Body.Close()
+
+	urldata, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("io read err", err)
+	}
+
+	err = json.Unmarshal(urldata, &cc)
+	if err != nil {
+		//fmt.Println("json decode error", err)
+	}
+
+	return cc, err
+}
+
+func getCCfromMovies(movie_list []Movie, person string, personB string) ([]CCtype, bool) {
+	cclist := make([]CCtype, 0)
+	var path Path
+
+	for _, val := range movie_list {
+		//fmt.Println("CastCrew", i, val)
+		cc, err := getCastCrew(val.Url)
+		if err != nil {
+			continue
+		}
+		cclist = append(cclist, cc.Cast...)
+		cclist = append(cclist, cc.Crew...)
+		for _, val2 := range cc.Cast {
+			path.name = person
+			path.movie = val
+			trace[val2.Url] = path
+			if val2.Url == personB {
+				return cclist, true
 			}
 		}
-		fmt.Println("Not in degree ", degree)
-		movie_list = getPersonMovieList(castcrew_list)
-		castcrew_list = getCastCrewList(movie_list)
+		for _, val2 := range cc.Crew {
+			path.name = person
+			path.movie = val
+			trace[val2.Url] = path
+			if val2.Url == personB {
+    			return cclist, true
+			}
+		}
+	}
+	return cclist, false
+}
+
+func getCCfromCC(person_list []CCtype, personB string) ([]CCtype, bool) {
+	cclist := make([]CCtype, 0)
+	var tmp Path
+
+	for _, person := range person_list {
+		//fmt.Println("Person", i, person)
+		mval, ok := processedPerson[person.Url]
+		if ok == false || mval != true {
+			movie_list, err := getPersonMovies(person.Url)
+			if err != nil {
+				continue
+			}
+			for _, movie := range movie_list {
+				mval, ok := processedMovie[movie.Url]
+				if ok == false || mval != true {
+					cc, _ := getCastCrew(movie.Url)
+					for _, val := range cc.Cast {
+						tmp.name = person.Url
+						tmp.movie = movie
+						trace[val.Url] = tmp
+						if val.Url == personB {
+							//fmt.Println("\n", val, personB, "\n")
+							return cclist, true
+						}
+					}
+					for _, val := range cc.Crew {
+						tmp.name = person.Url
+						tmp.movie = movie
+						trace[val.Url] = tmp
+						if val.Url == personB {
+							//fmt.Println("\n", val, personB, "\n")
+     						return cclist, true
+ 						}
+					}
+					cclist = append(cclist, cc.Cast...)
+					cclist = append(cclist, cc.Crew...)
+					processedMovie[movie.Url] = true
+				}
+			}
+		}
+		processedPerson[person.Url] = true
+	}
+	return cclist, false
+}
+
+func processPerson(personA string, personB string) {
+	degree = 1
+	movie_list, _ := getPersonMovies(personA)
+	castcrew_list, rc := getCCfromMovies(movie_list, personA, personB)
+	if rc == true {
+		fmt.Println("\nDegree of Seperation -", degree, "\n")
+		return 
+	}
+	for degree <= 6 {
 		degree++
+		castcrew_list, rc = getCCfromCC(castcrew_list, personB)
+		if rc == true {
+			fmt.Println("\nDegree of Seperation -", degree, "\n")
+			return
+		}
 	}
 	fmt.Println("Exceed the degree")
 }
 
 func main() {
 	args := os.Args
+	trace = make(map[string]Path)
+	processedMovie = make(map[string]bool)
+	processedPerson = make(map[string]bool)
 
 	if len(args) < 3 {
 		fmt.Println("Invalid Arguments")
@@ -155,4 +290,5 @@ func main() {
 	personA := args[1]
 	personB := args[2]
 	processPerson(personA, personB)
+	backtrace(personA, personB)
 }
