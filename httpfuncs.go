519964c650b0e9554a648cd5c4936ee6d6ba9d24
Sudarsan Govinda Reddy <sudarsang@virtusapolaris.com>
2016-09-13 16:14:31 +0530

    structure changes

--- httpfuncs.go
+++ httpfuncs.go
@@ -7,6 +7,12 @@ import (
 
 const moviebuff = "http://data.moviebuff.com/"
 
+var (
+	movieList map[string][]string
+	seen      map[string]bool
+	actors    []string
+)
+
 func ErrHandle(err error) {
 	if err != nil {
 		fmt.Println(err)
@@ -14,30 +20,47 @@ func ErrHandle(err error) {
 	}
 }
 
-func main(){
+func main() {
 	if len(os.Args) != 3 {
 		fmt.Print("Usage Example : degrees vn-mayekar magie-mathur")
 	}
+	seen = make(map[string]bool)
 	buildTree(os.Args[1], os.Args[2], os.Args[1], true)
 }
 
-func buildTree(argument, destination, parent string, getMovie bool ){
+func buildTree(argument, destination, parent string, getMovie bool) {
+	actors = []string{}
+	buildActors(argument, destination, parent)
+	fmt.Println(actors)
+}
 
+func buildActors(argument, destination, parent string) {
 	url := moviebuff + argument
 	json, err := getData(url)
 	defer ErrHandle(err)
 
-	if getMovie {
-		parent = argument
+	for _, movie := range json.Movies {
+		if isSeen(movie.Url) {
+			buildActors(movie.Url, destination, argument)
+		}
 	}
 
-	for _, movie := range json.Movies{
-		fmt.Println(movie.Url)
-		buildTree(movie.Url, destination, argument, false)
+	for _, cast := range json.Cast {
+		if isSeen(cast.Url) {
+			actors = append(actors, cast.Url)
+			if cast.Url == destination {
+				fmt.Println("DONE --> ", cast.Url)
+				os.Exit(1)
+			}
+		}
 	}
+}
 
-	for _, cast := range json.Cast{
-		fmt.Println(cast.Url)
+func isSeen(in string) bool {
+	if !seen[in] {
+		seen[in] = true
+		return true
+	} else {
+		return false
 	}
-
 }
