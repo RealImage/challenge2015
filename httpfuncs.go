624e7be73c9b913149dd62eb841fafeff36ba7c1
Sudarsan Govinda Reddy <sudarsang@virtusapolaris.com>
2016-09-15 12:56:02 +0530

    removed actorlist global

--- httpfuncs.go
+++ httpfuncs.go
@@ -25,53 +25,51 @@ func main() {
 		fmt.Print("Usage Example : degrees vn-mayekar magie-mathur")
 	}
 	seen = make(map[string]bool)
-	actorList = make(map[string][]string)
-	//var queue []string
-	if len(actorList) == 0 {
+	retList := make(map[string][]string)
+
+	degrees++
+	retList = loopMovies(os.Args[1], os.Args[1], os.Args[2])
+
+	if len(retList) != 0 {
 		degrees++
-		loopMovies(os.Args[1], os.Args[1], os.Args[2])
-	}
-	if len(actorList) != 0 {
-		for true {
-			degrees++
-			for _, v := range actorList[os.Args[1]] {
+		fmt.Println(len(retList))
+		for k := range retList {
+			for _, v := range retList[k] {
 				fmt.Println(v)
-				loopMovies(v,v, os.Args[2])
+				loopMovies(v, v, os.Args[2])
 			}
 		}
 	}
 }
 
-func buildTree(argument, destination string) {
-	//var retList map[string][]string
-	//retList[argument] = append(retList[argument]),loopMovies(argument, argument, destination))
-}
-
-func loopMovies(argument, parent, destination string) {
-	//var retList []string
+func loopMovies(argument, parent, destination string) map[string][]string {
+	retList := make(map[string][]string)
 	url := moviebuff + argument
 	json, err := getData(url)
 	defer ErrHandle(err)
 	for _, movie := range json.Movies {
 		if notSeen(movie.Url) {
-			loopActors(movie.Url, argument, destination)
+			retList[argument] = loopActors(movie.Url, argument,
+				destination, retList[argument])
 		}
 	}
+	return retList
 }
 
-func loopActors(argument, parent, destination string){
+func loopActors(argument, parent, destination string, retList []string) []string {
 	url := moviebuff + argument
 	json, err := getData(url)
 	defer ErrHandle(err)
 	for _, cast := range json.Cast {
 		if notSeen(cast.Url) {
-			actorList[parent] = append(actorList[parent], cast.Url)
+			retList = append(retList, cast.Url)
 			if cast.Url == destination {
 				fmt.Println("DONE --> ", cast.Url, degrees)
 				os.Exit(1)
 			}
 		}
 	}
+	return retList
 
 }
 
