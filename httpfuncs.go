ee1718562673e0ce02d5ab28928a5476628172a2
Sudarsan Govinda Reddy <sudarsang@virtusapolaris.com>
2016-09-16 12:32:30 +0530

    added a tracer struct and method

--- httpfuncs.go
+++ httpfuncs.go
@@ -8,11 +8,21 @@ import (
 const moviebuff = "http://data.moviebuff.com/"
 
 var (
-	seen      map[string]bool
-	degrees   int
-	destination string
+	seen    map[string]bool
+	degrees int
 )
 
+type traceData struct {
+	movie   string
+	childof map[string]string
+}
+
+func (t *traceData) addTrace(movie, parent, child string) {
+	t.movie = movie
+	t.childof[child] = parent
+}
+
+//General error panic
 func ErrHandle(err error) {
 	if err != nil {
 		fmt.Println(err)
@@ -20,36 +30,18 @@ func ErrHandle(err error) {
 	}
 }
 
-func main() {
-	if len(os.Args) != 3 {
-		fmt.Print("Usage Example : degrees vn-mayekar magie-mathur")
-	}
-	seen = make(map[string]bool)
-	retList := make(map[string][]string)
-	var q queue
-
-	degrees++
-	retList[os.Args[1]] = loopMovies(os.Args[1], os.Args[1], os.Args[2])
-
-	for k := range retList{
-		q.enqueue(k)
-	}
-	for len(q.value) != 0 {
-		degrees++
-		fmt.Println("Looking into level ",  degrees)
-		fmt.Println(q.value)
-		for _, k := range q.value{
-		q.dequeue()
-			for _, v := range retList[k] {
-				fmt.Println(v)
-				retList[v] = loopMovies(v, v, os.Args[2])
-				q.enqueue(v)
-			}
-		}
+func notSeen(in string) bool {
+	if !seen[in] {
+		seen[in] = true
+		return true
+	} else {
+		return false
 	}
 }
 
-func loopMovies(argument, parent, destination string) []string {
+func loopMovies(argument,
+	parent,
+	destination string) []string {
 	var retList []string
 	url := moviebuff + argument
 	json, err := getData(url)
@@ -63,7 +55,10 @@ func loopMovies(argument, parent, destination string) []string {
 	return retList
 }
 
-func loopActors(argument, parent, destination string, retList []string) []string {
+func loopActors(argument,
+	parent,
+	destination string,
+	retList []string) []string {
 	url := moviebuff + argument
 	json, err := getData(url)
 	defer ErrHandle(err)
@@ -89,11 +84,32 @@ func loopActors(argument, parent, destination string, retList []string) []string
 	return retList
 }
 
-func notSeen(in string) bool {
-	if !seen[in] {
-		seen[in] = true
-		return true
-	} else {
-		return false
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Print("Usage Example : degrees vn-mayekar magie-mathur")
+	}
+	seen = make(map[string]bool)
+	retList := make(map[string][]string)
+	var q queue
+
+	degrees++
+	retList[os.Args[1]] = loopMovies(os.Args[1], os.Args[1], os.Args[2])
+
+	/*Queue to employ BFS*/
+	for k := range retList {
+		q.enqueue(k)
+	}
+	for len(q.value) != 0 {
+		degrees++
+		fmt.Println("Looking into level ", degrees)
+		fmt.Println(q.value)
+		for _, k := range q.value {
+			q.dequeue()
+			for _, v := range retList[k] {
+				fmt.Println(v)
+				retList[v] = loopMovies(v, v, os.Args[2])
+				q.enqueue(v)
+			}
+		}
 	}
 }
