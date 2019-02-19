e03f39ea42128caaec2d602b109f484024ec0b3d
Naveen Honest Raj <naveen@skcript.com>
2019-02-19 23:04:21 +0530

    added a validation
    Signed-off-by: Naveen Honest Raj <naveen@skcript.com>

--- __init__.py
+++ __init__.py
@@ -14,6 +14,9 @@ if __name__ == '__main__':
   from_person = sys.argv[1]
   to_person = sys.argv[2]
 
+  if from_person == to_person:
+    sys.exit('Connection cannot be established to the same person')
+
   graph = Graph()
   graph.build_graph(from_person, to_person)
   graph.start_bfs(from_person, to_person)
